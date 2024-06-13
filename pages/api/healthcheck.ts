736698ada3c7af41a31f38b374bdd1543c244355
Ryan Li <ilnayr2003@gmail.com>
2024-06-13 12:00:00 -0400

    set up server

---
+++ pages/api/healthcheck.ts
@@ -0,0 +1,6 @@
+// pages/api/healthcheck.ts
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({ status: 'ok' });
+}
