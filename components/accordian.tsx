3438920f3b2e37b76a9e480ec273486ee65e9002
ryann-li <ilnayr2003@gmail.com>
2024-07-05 19:31:38 -0400

    update next version

--- components/accordian.tsx
+++ components/accordian.tsx
@@ -2,7 +2,14 @@ import Image, { StaticImageData } from "next/image";
 import { useState } from "react";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
-export default function Accordian(props: { title: string, children: string, className?: string, image: StaticImageData }) {
+interface AccordionProps {
+    title: string;
+    children: string;
+    className?: string;
+    image: StaticImageData;
+}
+
+export default function Accordion(props: AccordionProps) {
     const [open, setOpen] = useState(false);
 
     return (
@@ -10,12 +17,17 @@ export default function Accordian(props: { title: string, children: string, clas
             <div className={`bg-stone-600 items-center text-lg flex text-white p-3 cursor-pointer ${open ? "rounded-t-xl" : "rounded-xl"}`} onClick={e => setOpen(!open)}>
                 {props.title}
                 {open ? <AiOutlineMinus className='text-md ml-auto' /> : <AiOutlinePlus className='float-right ml-auto' />}
-
             </div>
             {open && <div className='p-3 border rounded-b-lg bg-white text-black'>
-                <Image alt="Accordian Image" className='w-1/3 mx-auto my-10 rounded-lg' src={props.image.src} />
+                <Image 
+                    alt="Accordion Image" 
+                    className='w-1/3 mx-auto my-10 rounded-lg' 
+                    src={props.image} 
+                    width={300} // Set appropriate width and height
+                    height={200} // Set appropriate width and height
+                />
                 <div className='w-2/3 mx-auto text-center'>{props.children}</div>
             </div>}
         </div>
-    )
+    );
 }
