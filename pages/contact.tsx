4033e990c898dec43ac421dccba01a1bf2ea17c1
ryann-li <ilnayr2003@gmail.com>
2024-07-07 18:00:18 -0400

    todo

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -10,6 +10,8 @@ export default function Contact() {
     const webapp_url: string = "kwwsv=22vfulsw1jrrjoh1frp2pdfurv2v2DNi|fe{Mxrd<5;pSXHZ7rpxKMuyxd;{m7V]s{33t6db0IPFHw8hWd9<pGfiq[W4bilb5sln2h{hf";
     const hashed: string = "kwwsv=22glvfrug1frp2dsl2zhekrrnv2<;357943;3<8:;5<762YEIZN6KxQOPkz0XDNyDL6L7q9Kh<sUVis{6GEFntDz5JNU[|V|{yfYDO4svZuDvl4VPG";
 
+    // TODO: put in API
+    
     return (
         <MUPage>
             <BlueWideDiv>
